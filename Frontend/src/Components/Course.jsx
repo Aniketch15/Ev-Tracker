3cac5e8abb770cf0de5cd53cd8a990777ffb0003
Aniketch <aniketchalake15@gmail.com>
2024-05-20 23:40:35 +0530

    Frontend Done!!

---
+++ Frontend/src/Components/Course.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Course() {
+  return (
+   
+    <div className="w-full md:w-1/2 mt-12 md:mt mt-32">
+        <div className="space-y-12">
+    <div>
+        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15081.162235407433!2d72.84740666977537!3d19.0949051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9e4a37a573d%3A0x298138813bc541!2sElectric%20Vehicle%20Charging%20Station!5e0!3m2!1sen!2sin!4v1716012582633!5m2!1sen!2sin" width="1500" height="950" style={{ border: "0" }}
+ allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+     >
+             </iframe>
+    </div>
+    </div>
+    </div>
+   
+  )
+}
+
+export default Course;
